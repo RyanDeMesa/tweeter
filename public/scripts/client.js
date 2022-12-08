515e0e35555b4d28c9e8525a093dcaf64ef4fcf1
RyanDeMesa <ryandemesa09@gmail.com>
2022-12-08 04:09:22 +0000

    Moved everything into document.ready to make things easier. Also created loadTwees function to get tweets from database and removed temp arr of objects

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,34 +1,5 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
+$(document).ready(() => {
+  
 const createTweetElement = (data) => {
   let $tweet = $(`
   <article>
@@ -60,15 +31,22 @@ const renderTweets = (tweets) => {
   }
 }
 
-$(document).ready(() => {
-  renderTweets(data)
-
-  $("#tweet-form").submit(function(event) {
-    event.preventDefault();
-    const serialized = $(this).serialize();
-    console.log(serialized);
-    $.post("/tweets", serialized);
+const loadTweets = () => {
+  $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
+    renderTweets(tweets)
+    console.log("Success: ", tweets);
   });
+};
+
+loadTweets();
+
+$("#tweet-form").submit(function(event) {
+  event.preventDefault();
+  const serialized = $(this).serialize();
+  console.log(serialized);
+  $.post("/tweets", serialized);
+});
+
 
 })
 
