75936051caae78d489a86656619ab110e38acfed
RyanDeMesa <ryandemesa09@gmail.com>
2022-12-08 00:39:40 +0000

    added functions to client.js to render tweets and fixed layout issues

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
+const createTweetElement = (data) => {
+  let $tweet = $(`
+  <article>
+  <header>
+    <div class="left-side">
+      <img class="image" src=${data.user.avatars}"></img>
+      <a>${data.user.name}</a>
+    </div>
+    <a class="right-side">${data.user.handle}</a>
+  </header>
+  <p>${data.content.text}</p>
+  <footer>
+    <time class="left-side">${data.created_at}</time>
+    <div class="icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+  </footer>
+  </article>`);
+  return $tweet;
+};
+
+const renderTweets = (tweets) => {
+  for (let index in tweets) {
+    const render = createTweetElement(tweets[index])
+  $('.tweets-container').append(render);
+  console.log(render);
+  }
+}
+
+
+
+$(document).ready(() => {renderTweets(data)})
